f9c8be12d34ad5976adafe0ce4476570638461cb
an_home <andreyrabota5@yandex.ru>
2020-04-27 22:57:02 +0300

    added speed param to ship and ship dont disappear near edge of a screen

--- settings.py
+++ settings.py
@@ -6,3 +6,5 @@ class Settings():
         self.screen_width = 1700
         self.screen_height = 900
         self.bg_color = (53, 84, 155)
+        #ship settings
+        self.ship_speed = 2
--- ship.py
+++ ship.py
@@ -7,25 +7,28 @@ class Ship():
         """sets ship starting position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
-
+        self.settings = ai_game.settings
         #loads ship image and recieves rectang
         self.image = pygame.image.load('images/spaceship.png')
         self.rect = self.image.get_rect()
         #ship starts at midbuttom
+        self.x = float(self.rect.x)
         self.rect.midbottom = self.screen_rect.midbottom
         self.moving_right = False
         self.moving_left = False
         self.moving_up = False
         self.moving_down = False
+
     def update(self):
-        if self.moving_right:
-            self.rect.x += 1
-        if self.moving_left:
-            self.rect.x -= 1
-        if self.moving_up:
-            self.rect.y -= 1
-        if self.moving_down:
-            self.rect.y += 1
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            print(self.rect.right, self.screen_rect.right)
+            self.rect.x += self.settings.ship_speed
+        if self.moving_left and self.rect.x > 0:
+            self.rect.x -= self.settings.ship_speed
+        if self.moving_up and self.rect.y > 0:
+            self.rect.y -= self.settings.ship_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.rect.y += self.settings.ship_speed
 
     def blitme(self):
         """draws ship in current position"""
